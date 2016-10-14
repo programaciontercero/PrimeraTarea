94da373a86fa1c1229eb5e9cddd2135bab3a96fa
FAMILIA OSUNA <FAMILIA OSUNA>
2016-10-13 19:50:43 -0700

    tarea trabajador

---
+++ trabajadortarea/ClassTrabajador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajadortarea
+{
+    class ClassTrabajador
+    {
+        public string Nombre;
+        public int Edad;
+        public double Salario;
+        public double Total;
+        public double iva;
+
+        public ClassTrabajador()
+        {
+            Nombre = "";
+            Edad = 0;
+            Salario = 0.0;
+        }
+    }
+}
---
+++ trabajadortarea/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajadortarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total;
+            string linea;
+            ClassTrabajador empleado = new ClassTrabajador();
+            
+            Console.WriteLine("Ingresa nombre: ");
+            empleado.Nombre = Console.ReadLine();
+         
+            Console.WriteLine("Ingresa Edad: ");
+            linea = Console.ReadLine();
+            empleado.Edad = int.Parse(linea);
+      
+            Console.WriteLine("Ingresa Salario: ");
+            linea = Console.ReadLine();
+            empleado.Salario = double.Parse(linea);
+
+            if (empleado.Salario >= 10000)
+            {
+                empleado.iva = 0.20 * empleado.Salario;
+                total = empleado.Salario - empleado.iva;
+                Console.WriteLine("tu salario es de : ");
+                Console.WriteLine(total);
+            }
+            else
+            {
+                empleado.iva = 0.16 * empleado.Salario;
+                total = empleado.Salario - empleado.iva;
+                Console.WriteLine("tu salario es de : {1}", total);
+                Console.WriteLine(total);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
